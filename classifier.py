205b4032c5d9454408b89fcd57346014d35e236a
HiddevdBos <hiddevandenbos@upcmail.nl>
2021-02-06 13:47:20 +0100

    load data

--- classifier.py
+++ classifier.py
@@ -1,5 +1,21 @@
 import zipfile
 
-with zipfile.ZipFile('data.zip') as data:
-    training_data = data.open('fashion-mnist_train.csv')
-    test_data = data.open('fashion-mnist_test.csv')
+def load_data(path):
+    with zipfile.ZipFile('data.zip') as data:
+        data_file = data.open(path)
+    
+    data =[]
+    labels = []
+    for idx, line in enumerate(data_file):
+        if idx == 0:
+            continue
+        line = str(line)[:-3]
+        image = line.split(',')
+        labels.append(image[0][2:])
+        image = list(map(int, image[1:]))
+        data.append(image)
+    return data, labels
+
+
+training_data, training_labels = load_data('fashion-mnist_train.csv')
+test_data, test_labels = load_data('fashion-mnist_test.csv')
