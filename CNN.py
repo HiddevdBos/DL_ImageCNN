2be020dd2210f31fae6305d974c3268b48e5d8a3
CorineNijhof <c.nijhof@student.rug.nl>
2021-02-11 17:48:12 +0100

    added (still faulty) training and testing procedure

--- CNN.py
+++ CNN.py
@@ -2,6 +2,8 @@ import torch
 from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
 from torch.nn import Linear, ReLU, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
+from tqdm import tqdm
+import numpy as np
 
 
 class CNN(Module):
@@ -28,37 +30,44 @@ class CNN(Module):
         x = self.linear_layers(x)
         return x
 
-def train_model(images, labels, epochs=20, learningRate=0.007):
-    pass
-    # x, y = matrices_to_tensors(x, y)
-    # model = Net()
-    # model = model.float()
-    # optimizer = Adam(model.parameters(), lr=learningRate, weight_decay=l2_weight_decay)
-    # criterion = CrossEntropyLoss()
-    # loss_list = []
-    # # batch sizes are calculated and sorted
-    # if not batch_size or batch_size > x.shape[0]:
-    #     batch_size = x.shape[0]
-    # batch_num = x.shape[0] / batch_size
-    # x = x.reshape(-1, batch_size, 1, 16, 15)
-    # y = y.reshape(-1, batch_size)
-    #
-    # for epoch in range(0, epochs):
-    #     # loop over the number of batches feeds in batch_size many images and performs backprop
-    #     # then again and so on
-    #     for i in range(0, int(batch_num)):
-    #         # here we feed in training data and perform backprop according to the loss
-    #         # run the forward pass
-    #         outputs = model.forward(x[i])
-    #         y = y.long()
-    #         loss = criterion(outputs, y[i])
-    #         loss_list.append(loss.item())
-    #
-    #         # backprop and perform Adam optimisation
-    #         optimizer.zero_grad()
-    #         loss.backward()
-    #         optimizer.step()
-    # return model, loss_list
 
-def test_model():
-    pass
+def train_model(train_x, train_y, epochs=20):
+    model = CNN()
+    # model = model.float()
+    optimizer = Adam(model.parameters())
+    criterion = CrossEntropyLoss()
+
+    loss_list = []
+    for epoch in tqdm(range(0, epochs)):
+        for i in range(len(train_x)):
+            outputs = model.forward(train_x[i])
+            train_y = train_y.long()
+            loss = criterion(outputs, train_y[i])
+            loss_list.append(loss.item())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    return model, np.mean(loss_list)
+
+
+def eval_cnn(test_x, test_y, model):
+    output = model.forward(test_x)
+    total = test_y.size(0)
+    _, predicted = torch.max(output.data, 1)
+    correct = 0
+    for i in range(total):
+        if predicted[i] == test_y[i]:
+            correct += 1
+    return correct / total
+
+
+def train_and_test_model(train_x, train_y, test_x, test_y, n=5):
+    train_acc = []
+    test_acc = []
+    for i in tqdm(range(n)):
+        model, loss = train_model(train_x, train_y)
+        train_acc.append(1-loss)
+        acc = eval_cnn(test_x, test_y, model)
+        test_acc.append(acc)
+    return np.mean(train_acc), np.mean(test_acc)
--- launch.py
+++ launch.py
@@ -1,12 +1,13 @@
 from readData import get_data
-from CNN import test_model
+from CNN import train_and_test_model
 
-
-train_data, train_labels = get_data('fashion-mnist_train.csv', plot=False)
-print(train_data.size())
+train_images, train_labels = get_data('fashion-mnist_train.csv', plot=False)
+print(len(train_images))
+print(train_images.size())
 print(train_labels.size())
-test_data, test_labels = get_data('fashion-mnist_test.csv')
-print(test_data.size())
+test_images, test_labels = get_data('fashion-mnist_test.csv')
+print(test_images.size())
 print(test_labels.size())
 
-
+train_acc, test_acc = train_and_test_model(train_images, train_labels, test_images, test_labels)
+print(train_acc, test_acc)
