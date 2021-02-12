f9ea60cabe096889129cc9875da3afd0d98ebe39
CorineNijhof <c.nijhof@student.rug.nl>
2021-02-12 13:06:49 +0100

    extended (still faulty) training procedure

--- CNN.py
+++ CNN.py
@@ -9,37 +9,52 @@ import numpy as np
 class CNN(Module):
     def __init__(self):
         super(CNN, self).__init__()
+        self.input_size_linear = None
+
         self.cnn_layers = Sequential(
-            # Defining a 2D convolution layer
+            # 2D convolution layer
             Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
             BatchNorm2d(4),
             ReLU(inplace=True),
             MaxPool2d(kernel_size=2, stride=2),
-            # Defining another 2D convolution layer
+            # 2D convolution layer
             Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
             BatchNorm2d(4),
             ReLU(inplace=True),
             MaxPool2d(kernel_size=2, stride=2),
         )
-        self.linear_layers = Sequential(Linear(4 * 7 * 7, 10))
+
+        x = torch.randn(28, 28).view(-1, 1, 28, 28)
+        self.cnn_layers(x)
+        self.input_size_linear = x.shape[1] * x.shape[2] * x.shape[3]
+
+        self.linear_layers = Sequential(
+            Linear(self.input_size_linear, 10)
+        )
 
     # Defining the forward pass
     def forward(self, x):
         x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
+        x = x.view(-1, self.input_size_linear)
         x = self.linear_layers(x)
         return x
 
 
-def train_model(train_x, train_y, epochs=20):
+def train_model(train_x, train_y, epochs=20, learning_rate=0.01, weight_decay=0.01, batch_size=None):
+    print(train_y)
     model = CNN()
-    # model = model.float()
-    optimizer = Adam(model.parameters())
+    model = model.float()
+    optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     criterion = CrossEntropyLoss()
+    if not batch_size or batch_size > train_x.shape[0]:
+        batch_size = train_x.shape[0]
+    num_batches = train_x.shape[0] / batch_size
+    train_x = train_x.reshape(-1, batch_size, 1, 28, 28)
+    train_y = train_y.reshape(-1, batch_size)
 
     loss_list = []
     for epoch in tqdm(range(0, epochs)):
-        for i in range(len(train_x)):
+        for i in range(train_x.shape[0]):
             outputs = model.forward(train_x[i])
             train_y = train_y.long()
             loss = criterion(outputs, train_y[i])
@@ -65,9 +80,10 @@ def eval_cnn(test_x, test_y, model):
 def train_and_test_model(train_x, train_y, test_x, test_y, n=5):
     train_acc = []
     test_acc = []
-    for i in tqdm(range(n)):
+    for i in range(n):
         model, loss = train_model(train_x, train_y)
-        train_acc.append(1-loss)
+        train_acc.append(1 - loss)
         acc = eval_cnn(test_x, test_y, model)
+        print(acc)
         test_acc.append(acc)
     return np.mean(train_acc), np.mean(test_acc)
--- launch.py
+++ launch.py
@@ -2,12 +2,11 @@ from readData import get_data
 from CNN import train_and_test_model
 
 train_images, train_labels = get_data('fashion-mnist_train.csv', plot=False)
-print(len(train_images))
-print(train_images.size())
-print(train_labels.size())
+# print(train_images.size())
+# print(train_labels.size())
 test_images, test_labels = get_data('fashion-mnist_test.csv')
-print(test_images.size())
-print(test_labels.size())
+# print(test_images.size())
+# print(test_labels.size())
 
 train_acc, test_acc = train_and_test_model(train_images, train_labels, test_images, test_labels)
 print(train_acc, test_acc)
